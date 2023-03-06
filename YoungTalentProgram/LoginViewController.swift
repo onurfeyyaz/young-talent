ee20bfc877af2e95c2c37d2b8423aeeaef0e78d3
onurfeyyaz <feyyazonur0@gmail.com>
2023-03-06 21:48:25 +0300

    add dark mode

--- YoungTalentProgram/LoginViewController.swift
+++ YoungTalentProgram/LoginViewController.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupUI() {
+        phoneNumberTextField.borderStyle = .none
+        phoneNumberTextField.layer.cornerRadius = 17.0
     }
-    */
-
 }
